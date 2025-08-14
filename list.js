c4e2c7735cbafa617177e7180c3d85bf3abda6b9
Adilson Cardoso <adilbcardoso@gmail.com>
2025-08-14 01:35:58 -0400

    my first commit

---
+++ list.js
@@ -0,0 +1,14 @@
+const capeVerdIsland = [
+  "Santo Antão",
+  "São Nicolau",
+  "São Vicente",
+  "Santa Luzia",
+  "Sal",
+  "Boa Vista",
+  "Santiago",
+  "Maio",
+  "Fogo",
+  "Brava",
+];
+
+console.log(capeVerdIsland);
